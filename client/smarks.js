c4ba39a6ce1023f4f2d01cb8efb8899b32d507e3
nada <flashpogo@gmail.com>
2012-10-26 11:50:18 +0200

    introduced embed.ly, removed autopublish and insecure packages

--- client/smarks.js
+++ client/smarks.js
@@ -9,21 +9,21 @@ var okCancelEvents = function (selector, callbacks) {
 
   var events = {};
   events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
-    function (evt) {
-      if (evt.type === "keydown" && evt.which === 27) {
-        // escape = cancel
-        cancel.call(this, evt);
+	function (evt) {
+	  if (evt.type === "keydown" && evt.which === 27) {
+		// escape = cancel
+		cancel.call(this, evt);
 
-      } else if (evt.type === "keyup" && evt.which === 13 ||
-                 evt.type === "focusout") {
-        // blur/return/enter = ok/submit if non-empty
-        var value = String(evt.target.value || "");
-        if (value)
-          ok.call(this, value, evt);
-        else
-          cancel.call(this, evt);
-      }
-    };
+	  } else if (evt.type === "keyup" && evt.which === 13 ||
+				 evt.type === "focusout") {
+		// blur/return/enter = ok/submit if non-empty
+		var value = String(evt.target.value || "");
+		if (value)
+		  ok.call(this, value, evt);
+		else
+		  cancel.call(this, evt);
+	  }
+	};
   return events;
 };
 
@@ -35,51 +35,77 @@ var activateInput = function (input) {
 // ----------------main
 
 
-if (Meteor.isClient) {
-  
+Meteor.subscribe("smarks");
 
-/*
-  Template.welcome.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
+
+Template.timeline.posts = function() {
+  return Smarks.find({}, {sort: {timestamp:-1}});
+};
+
+
+Template.page.events(okCancelEvents(
+'#new-smark',
+{
+  ok: function (text, evt) {
+
+	Smarks.insert({
+	  avatar: Meteor.user().profile.name,
+	  smark: text,
+	  owner: Meteor.userId(),
+	  timestamp:new Date().getTime()
+	});
+
+	evt.target.value = '';
+  }
+}));
+
+var linkify = function(text) {
+	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+	return text.replace(exp,"<a href='$1'>$1</a>"); 
+}
+
+Template.page.rendered = function () {
+  $('.smark').each(function(){
+	$(this).html(linkify($(this).text()));
   });
-*/
-
-  Template.timeline.posts = function() {
-    return Smarks.find({}, {sort: {timestamp:-1}});
-  };
-
-
-  Template.page.events(okCancelEvents(
-  '#new-smark',
-  {
-    ok: function (text, evt) {
-      /*
-      console.log(text);
-      console.log(evt);
-      console.log(Meteor.user());
-      console.log(Meteor.userId());
-      */
-console.log(Meteor.user().profile);
-
-      Smarks.insert({
-        avatar: Meteor.user().profile.name,
-        smark: text,
-        owner: Meteor.userId(),
-        timestamp:new Date().getTime()
-      });
-  
-      evt.target.value = '';
-    }
-  }));
-
-
+  $('div.smark').not('.embedlied').embedly({
+	maxWidth: '300px',
+	maxHeight: '200px',
+	wmode: 'transparent',
+	method: 'afterParent',
+	chars: 100,
+	className : "embedly span5 offset2",
+	key: 'f8fe34981bf2459e850c443dd1e587b7'
+  }).addClass("embedlied");
 }
 
 
+/*
+var filterURLsFrom = function (smarktext)
+{
+  var urlRegEx = /(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/gi;
+  var res = smarktext.search(urlRegEx);
+  res = "hello";
+  return res;
+};
+*/
 
+/* embedly */
+/*
+Template.post.rendered = function () {
+  //var res = filterURLsFrom(this.smark);
+  console.log(res);
+  
+  $('div.smark').embedly({
+	maxWidth: '300px',
+	maxHeight: '200px',
+	wmode: 'transparent',
+	method: 'after',
+	chars: 100,
+	key: 'f8fe34981bf2459e850c443dd1e587b7'
+  });
+
+}
+*/
 
 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+Meteor.startup(function () {
+// code to run on server at startup
 
     //fill one post in if empty
     if (Smarks.find().count() === 0) {
-    	Smarks.insert({
-    		avatar:"Admin",
-    		smark:"Hello World this is your post", 
-    		owner:"admin",
-    		timestamp:new Date().getTime()
-    	});
+        Smarks.insert({
+        	avatar:"Admin",
+        	smark:"Hello World this is your post", 
+        	owner:"admin",
+        	timestamp:new Date().getTime()
+        });
     }
+});
 
-  });
-}
+Meteor.publish("smarks", function () {
+  return Smarks.find({}, {sort: {timestamp: -1}});
+});
