bab5b82b6a44a3a34192f869da8a9f4348f57d21
nada <flashpogo@gmail.com>
2012-10-27 22:36:33 +0200

    bulk commit, sorry no nice history

--- client/smarks.js
+++ client/smarks.js
@@ -32,53 +32,136 @@ var activateInput = function (input) {
   input.select();
 };
 
+var BOX_WIDTH = 300;
+var BOX_OFFSET = 10;
+var repositionPosts = function(event) {
+	//calculate new posts
+	var postCols = Math.floor(($('.timeline').width() + BOX_OFFSET) / (BOX_WIDTH + BOX_OFFSET));
+	var postRowPointer = new Array(postCols);
+	for(var i = 0; i < postCols; i++)
+	{
+		postRowPointer[i] = 0;
+	}
+	//loop through posts and reposition em.
+	$('.timeline .box.smark').each(function(){
+		//get the lowest rowpointer
+		var min = Math.min.apply(Math, postRowPointer);
+		//get fist appearence of min
+		for(var j = 0; j < postCols; j++)
+		{
+			if(postRowPointer[j] == min)
+			{
+				$(this).css('left', (j * (BOX_WIDTH + BOX_OFFSET)) + "px");
+				$(this).css('top', postRowPointer[j] + "px");
+				postRowPointer[j] = $(this).position().top + $(this).height() + BOX_OFFSET;
+				break;
+			}
+		}
+	});
+	var max = Math.max.apply(Math, postRowPointer);
+	$(".page").css('height', ($('.timeline').position().top + max + 60) + "px");
+}
+
 // ----------------main
 
-
-Meteor.subscribe("smarks");
+Meteor.startup(function () {
+	Meteor.subscribe("smarks");
+	$(window).resize(repositionPosts);
+});
 
 
 Template.timeline.posts = function() {
-  return Smarks.find({}, {sort: {timestamp:-1}});
+	return Smarks.find({}, {sort: {timestamp:-1}}); 
 };
 
+Template.post.helpers({
+  formatdate: function (object) {
+  	var diff = new Date().getTime() - object;
+  	var m = Math.floor(diff / 1000 / 60);
+  	var mm = m % 60;
+  	var h = Math.floor(m / 60);
+  	var hh = h % 60;
+  	var dd = Math.floor(h / 24);
+
+  	var str = "";
+  	if(dd > 0) {
+  		if(dd == 1) str += dd + " day ago";
+  		else str += dd + " days ago";
+  	}
+  	else if(hh > 0) {
+  		if(hh == 1) str += hh + " hour ago";
+  		else str += hh + " hours ago";
+  	}
+  	else {
+  		if(mm == 1) str += mm + " minute ago";
+  		else str += mm + " minutes ago";
+  	}
+    return str;
+  }
+});
 
 Template.page.events(okCancelEvents(
-'#new-smark',
-{
-  ok: function (text, evt) {
+	'#new-smark',
+	{
+	  ok: function (text, evt) {
 
-	Smarks.insert({
-	  avatar: Meteor.user().profile.name,
-	  smark: text,
-	  owner: Meteor.userId(),
-	  timestamp:new Date().getTime()
-	});
+		Smarks.insert({
+		  avatar: Meteor.user().profile.name,
+		  smark: text,
+		  owner: Meteor.userId(),
+		  timestamp:new Date().getTime()
+		});
 
-	evt.target.value = '';
-  }
-}));
+		evt.target.value = '';
+	  }
+	})
+);
 
 var linkify = function(text) {
 	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
-	return text.replace(exp,"<a href='$1'>$1</a>"); 
+	urls = text.match(exp);
+	text = text.replace(exp,"<a class='link' href='$1'>$1</a>");
+	return {urls:urls, text:text}; 
 }
 
+//on init page load, this runs 2 times, can't figure out why...
 Template.page.rendered = function () {
-  $('.smark').not('.linkified').each(function(){
-	$(this).html(linkify($(this).text()));
-  }).addClass("linkified");
-  $('div.smark').not('.embedlied').embedly({
-	maxWidth: '300px',
-	maxHeight: '200px',
-	wmode: 'transparent',
-	method: 'afterParent',
-	chars: 100,
-	className : "embedly span5 offset2",
-	key: 'f8fe34981bf2459e850c443dd1e587b7'
-  });
-  //do this after the loop to lock the smarks from embedlying
-  $('div.smark').not('.embedlied').addClass("embedlied");
+	//go through every post and linkify and ebedly it
+	$('.smark .inner').not('.linkified').each(function(){
+		var res = linkify($(this).text());
+		$(this).html(res.text);
+		console.log(res.text);
+		if(res.urls != null)
+		{
+			var node = this;
+			$.embedly(res.urls, {
+					key:'f8fe34981bf2459e850c443dd1e587b7',
+					maxWidth: '260px', 
+					maxHeight: '260px'
+				}, function(oembed, dict){
+	   				switch(oembed.type)
+					{
+						case "photo":
+							$(node).append('<div class="embedly"><img src="' + oembed.url + '" width="'+oembed.width+'" height="'+oembed.height+'"/></div>');
+							break;
+						case "link":
+							if(oembed.thumbnail_url) {
+								$(node).append('<div class="embedly"><img src="' + oembed.thumbnail_url + '" width="'+oembed.thumbnail_width+'" height="'+oembed.thumbnail_height+'"/></div>'); 	
+							}
+							break;
+						case "video":
+						case "rich":
+							$(node).append('<div class="embedly" style="width:'+oembed.width+'px;height:'+oembed.height+'px;">' + oembed.html + '</div>');
+							break;
+						default:
+							break;
+					}
+					repositionPosts();  				
+				}
+			);
+		}
+  	}).addClass("linkified");
+  	repositionPosts();
 }
 
 
